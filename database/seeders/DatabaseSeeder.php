834697ed4af6431b818901d5d709fff148d68f2f
Kevin <kevinmonteiro2014@live.com>
2023-07-22 22:33:26 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,197 +2,76 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Atpv;
 use App\Models\Cliente;
 use App\Models\Despachante;
 use App\Models\Endereco;
 use App\Models\Pedido;
-use App\Models\PedidoServico;
-use App\Models\Plano;
 use App\Models\Processo;
 use App\Models\Servico;
 use App\Models\User;
-use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        $faker = Faker::create('pt_BR');
+        $quantidadeDespachantes = 3;
+        $quantidadeServicosPorDespachante = 3;
+        $quantidadeClientesPorDespachante = 3;
+        $quantidadePedidosPorCliente = 5;
+        $quantidadeAtpvPorPedido = 1;
 
-        $plano = Plano::create([
-            'nome' => 'Plano 1',
-            'preco' => 100,
-            'descricao' => 'Descrição 1',
-            'qtd_clientes' => 100,
-            'qtd_acessos_clientes' => 10,
-        ]);
+        Despachante::factory($quantidadeDespachantes)->create()->each(function ($despachante) use ($quantidadeServicosPorDespachante, $quantidadeClientesPorDespachante, $quantidadePedidosPorCliente, $quantidadeAtpvPorPedido) {
+            $despachante->endereco_id = Endereco::factory()->create()->id;
+            $despachante->save();
 
-        for ($i = 0; $i < 3; $i++) {
-            $endereco = Endereco::create([
-                'logradouro' => $faker->streetName,
-                'numero' => $faker->buildingNumber,
-                'bairro' => $faker->citySuffix,
-                'cidade' => $faker->city,
-                'estado' => $faker->stateAbbr,
-                'cep' => $faker->postcode,
+            Servico::factory($quantidadeServicosPorDespachante)->create([
+                'despachante_id' => $despachante->id,
             ]);
 
-            $despachantes = Despachante::create([
-                'razao_social' => $faker->company,
-                'cnpj' => $faker->numerify('##############'),
-                'endereco_id' => $endereco->id,
-                'plano_id' => $plano->id,
+            $user = User::factory()->create([
+                'despachante_id' => $despachante->id,
             ]);
-        }
 
-
-<<<<<<< Updated upstream
-        $pedidos = [];
-        $qtd_pedidos = 30;
-        for($i = 0; $i < $qtd_pedidos; $i++) {
-            $pedidos[] = [
-                'comprador_nome' => $faker->name,
-                'comprador_telefone' => $faker->cellphoneNumber,
-                'placa' => $faker->numerify('#######'),
-                'veiculo' => $faker->word,
-                'preco_placa' => $faker->randomFloat(2, 100, 1000),
-                'preco_honorario' => $faker->randomFloat(2, 100, 1000),
-                'status' => $faker->randomElement(['ab', 'ea', 'cd']),
-                'criado_por' => 1,
-                'cliente_id' => 1,
-            ];
-        }
-        foreach ($pedidos as $pedido) {
-            $pedidoNovo = Pedido::create($pedido);
-            Atpv::create([
-                'renavam' => $faker->numerify('###########'),
-                'numero_crv' => $faker->numerify('############'),
-                'vendedor_email' => $faker->email,
-                'vendedor_telefone' => $faker->cellphoneNumber,
-                'vendedor_cpf_cnpj' => $faker->numerify('##############'),
-                'comprador_cpf_cnpj' => $faker->numerify('##############'),
-                'comprador_email' => $faker->email,
-                'comprador_endereco' => 1,
-                'preco_venda' => $faker->randomFloat(2, 100, 1000),
-                'pedido_id' => $pedidoNovo->id,
-            ]);
-        }
-
-
-
-//        $processos = [];
-//        $pedidos = Pedido::all();
-//        foreach ($pedidos as $pedido) {
-//            $processos[] = [
-//                'tipo' => $faker->randomElement(['ss', 'rv']),
-//                'comprador_tipo' => $faker->randomElement(['lj', 'tc']),
-//                'qtd_placas' => $faker->randomDigit,
-//                'pedido_id' => $pedido->id,
-//            ];
-//        }
-//        foreach ($processos as $processo) {
-//            Processo::create($processo);
-//        }
-=======
-        foreach (Despachante::all() as $item) {
-            for ($j = 0; $j < 3; $j++) {
-                $servico = Servico::create([
-                    'nome' => $faker->word,
-                    'preco' => $faker->randomFloat(2, 100, 1000),
-                    'descricao' => $faker->text,
-                    'despachante_id' => $item->id,
+            Cliente::factory($quantidadeClientesPorDespachante)->create([
+                'despachante_id' => $despachante->id,
+            ])->each(function ($cliente) use ($user, $quantidadePedidosPorCliente, $quantidadeAtpvPorPedido) {
+                User::factory()->create([
+                    'role' => 'ca',
+                    'cliente_id' => $cliente->id,
                 ]);
->>>>>>> Stashed changes
 
-                $user = User::create([
-                    'name' => $faker->name,
-                    'email' => $faker->email,
-                    'password' => Hash::make('123'),
-                    'role' => $faker->randomElement(['da', 'du']),
-                    'despachante_id' => $item->id,
-                ]);
-                for ($p = 0; $p < 3; $p++) {
-                    $cliente = Cliente::create([
-                        'nome' => $faker->company,
-                        'status' => $faker->randomElement(['ac', 'in']),
-                        'preco_1_placa' => $faker->randomFloat(2, 100, 1000),
-                        'preco_2_placa' => $faker->randomFloat(2, 100, 1000),
-                        'preco_atpv' => $faker->randomFloat(2, 100, 1000),
-                        'preco_loja' => $faker->randomFloat(2, 100, 1000),
-                        'preco_terceiro' => $faker->randomFloat(2, 100, 1000),
-                        'despachante_id' => $item->id,
-                    ]);
-
-                    User::create([
-                        'name' => $faker->name,
-                        'email' => $faker->email,
-                        'password' => Hash::make('123'),
-                        'role' => 'ca',
-                        'cliente_id' => $cliente->id,
-                    ]);
-
-                    for ($k = 0; $k < 5; $k++) {
-                        $pedido = Pedido::create([
-                            'comprador_nome' => $faker->name,
-                            'comprador_telefone' => $faker->cellphoneNumber,
-                            'placa' => $faker->numerify('#######'),
-                            'veiculo' => $faker->word,
-                            'preco_placa' => $faker->randomFloat(2, 100, 1000),
-                            'preco_honorario' => $faker->randomFloat(2, 100, 1000),
-                            'status' => $faker->randomElement(['ab', 'ea', 'pe', 'co']),
-                            'criado_por' => $user->id,
-                            'cliente_id' => $cliente->id,
-                        ]);
-
-                        Atpv::create([
-                            'renavam' => $faker->numerify('###########'),
-                            'numero_crv' => $faker->numerify('############'),
-                            'vendedor_email' => $faker->email,
-                            'vendedor_telefone' => $faker->cellphoneNumber,
-                            'vendedor_cpf_cnpj' => $faker->numerify('##.###.###/####-##'),
-                            'comprador_cpf_cnpj' => $faker->numerify('###.###.###-##'),
-                            'comprador_email' => $faker->email,
-                            'comprador_endereco' => $endereco->id,
-                            'preco_venda' => $faker->randomFloat(2, 100, 1000),
+                Pedido::factory($quantidadePedidosPorCliente)->create([
+                    'criado_por' => $user->id,
+                    'cliente_id' => $cliente->id,
+                ])->each(function ($pedido) use ($cliente, $quantidadeAtpvPorPedido) {
+                    for ($i = 0; $i < $quantidadeAtpvPorPedido; $i++) {
+                        Atpv::factory()->create([
+                            'comprador_endereco_id' => Endereco::factory()->create()->id,
                             'pedido_id' => $pedido->id,
                         ]);
                     }
-                    for ($t = 0; $t < 5; $t++) {
-                        $pedido = Pedido::create([
-                            'comprador_nome' => $faker->name,
-                            'comprador_telefone' => $faker->cellphoneNumber,
-                            'placa' => $faker->numerify('#######'),
-                            'veiculo' => $faker->word,
-                            'preco_placa' => $faker->randomFloat(2, 100, 1000),
-                            'preco_honorario' => $faker->randomFloat(2, 100, 1000),
-                            'status' => $faker->randomElement(['ab', 'ea', 'pe', 'co']),
-                            'criado_por' => $user->id,
-                            'cliente_id' => $cliente->id,
-                        ]);
-
-                        Processo::create([
-                            'tipo' => $faker->randomElement(['ss', 'rv']),
-                            'comprador_tipo' => $faker->randomElement(['lj', 'tc']),
-                            'qtd_placas' => $faker->randomElement([0, 1, 2]),
+                });
+                Pedido::factory($quantidadePedidosPorCliente)->create([
+                    'criado_por' => $user->id,
+                    'cliente_id' => $cliente->id,
+                ])->each(function ($pedido) use ($quantidadeAtpvPorPedido) {
+                    for ($i = 0; $i < $quantidadeAtpvPorPedido; $i++) {
+                        Atpv::factory()->create([
                             'pedido_id' => $pedido->id,
-                        ]);
-
-                        PedidoServico::create([
-                            'pedido_id' => $pedido->id,
-                            'servico_id' => $servico->id,
                         ]);
                     }
-                }
-            }
-        }
 
+                    Processo::factory()->create([
+                        'pedido_id' => $pedido->id,
+                    ]);
+                });
+            });
+
+
+        });
 
         User::create([
             'name' => 'admin',
@@ -201,6 +80,9 @@ class DatabaseSeeder extends Seeder
             'role' => 'dm',
             'despachante_id' => Despachante::first()->id,
         ]);
-
+        User::factory()->create([
+            'role' => 'ca',
+            'cliente_id' => Cliente::first()->id,
+        ]);
     }
 }
